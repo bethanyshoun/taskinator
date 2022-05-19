0990f684a0e0df62071d7abf82815874ed92f570
bethanyshoun <bethanyshoun@gmail.com>
2022-05-19 17:55:36 -0400

    completed capture data form and edit ability

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,15 +16,35 @@ var taskFormHandler = function(event) {
         return false;
     }
     formEl.reset();
+
+    var isEdit = formEl.hasAttribute("data-task-id");
+    
+    //has data attribute, so get task id and call function to complete edit process
+    if (isEdit) {
+        var taskId = formEl.getAttribute("data-task-id");
+        completeEditTask(taskNameInput, taskTypeInput, taskId);
+    }
+
+    //no data attribute, so create object as normal and pass to createTaskEl function
+    else {
+        var taskDataObj = {
+            name: taskNameInput,
+            type: taskTypeInput
+        };
+
+        //send it as an argument to createTaskEl
+        createTaskEl(taskDataObj);
+    }
+
     // package up data as an object
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput,
     };
 
-    //send it as an argument to createTaskEl
-    createTaskEl(taskDataObj);
 };
+
+
 
 var createTaskEl = function(taskDataObj) {
     // create list item
@@ -109,6 +129,21 @@ var taskButtonHandler = function(event) {
         deleteTask(taskId);
     }
 };
+
+var completeEditTask = function(taskName, taskType, taskId) {
+    //find the matching task list item
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    //set new values
+    taskSelected.querySelector("h3.task-name").textContent = taskName;
+    taskSelected.querySelector("span.task-type").textContent = taskType;
+
+    alert("Task Updated!");
+
+    formEl.removeAttribute("data-task-id");
+    document.querySelector("#save-task").textContent = "Add Task";
+};
+
 
 var editTask = function(taskId) {
 console.log("editing task #" + taskId);
